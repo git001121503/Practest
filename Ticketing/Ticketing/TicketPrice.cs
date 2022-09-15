aedda40831b1d6fc074354237895228bece4f2d7
Minh Ong <minh.ong@student.tafesa.edu.au>
2022-09-15 21:10:11 +0930

    Modified ticket Price .xaml + 10% discount

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -16,7 +16,7 @@ namespace Ticketing
         const decimal mdecBalcony = 36.5m;
         const decimal mdecGeneral = 29.75m;
         const decimal mdecBox = 62.0m;
-        const decimal mdecDiscount = 5.0m; 
+        const decimal mdecDiscount = 15.0m; 
 
         private int Section
         {
